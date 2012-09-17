61b71ce28771311ecc8c2a91e66e2383ba564e8b
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2012-09-17 18:15:21 +0300

    Turns on semantic mode and semantic stickyfunc-mode for c-mode.

--- personal/ui.el
+++ personal/ui.el
@@ -1,9 +1,16 @@
 ;;; fonts
 (set-face-attribute 'default nil :height 90)
 
-;;; theme
 
+;;; theme
 (defvar prelude-personal-themes-dir (concat prelude-dir "themes/"))
 (add-to-list 'custom-theme-load-path prelude-personal-themes-dir)
 
 (load-theme 'aisbaa-zenburn t)
+
+
+;;; c-mode
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (semantic-mode t)
+            (semantic-stickyfunc-mode t)))
