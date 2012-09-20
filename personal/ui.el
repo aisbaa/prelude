d245ffcdc98d03f573bfe3d15204c16a602e8663
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2012-09-20 13:09:27 +0300

    Disables global linum mode, enables linum for c and lisp modes.

--- personal/ui.el
+++ personal/ui.el
@@ -9,8 +9,33 @@
 (load-theme 'aisbaa-zenburn t)
 
 
+;;; line numbers
+(global-linum-mode -1)
+
+
+;;; speed bar
+(require 'sr-speedbar)
+
+(custom-set-variables
+  '(speedbar-show-unknown-files t))
+
+(global-set-key (kbd "C-c C-t") 'sr-speedbar-toggle)
+
+
 ;;; c-mode
-(add-hook 'c-mode-common-hook
+(add-hook 'c-mode-hook
           (lambda ()
+            (linum-mode t)
             (semantic-mode t)
             (semantic-stickyfunc-mode t)))
+
+
+;;; lisp-mode
+
+;;; adding line number to each lisp mode
+(dolist (hook '(lisp-mode-hook
+                common-lisp-mode-hook
+                emacs-lisp-mode-hook
+                lisp-interaction-mode-hook))
+  (add-hook hook (lambda ()
+                   (linum-mode t))))
