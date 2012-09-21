d5089a09403ea5b758083852ffce265cd056f6f0
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2012-09-21 17:16:46 +0300

    Adds redo+, evil-numbers and ascope to prelude-packages.

---
+++ personal/package.el
@@ -0,0 +1,9 @@
+;; My packages
+(setq prelude-packages (append '(redo+
+                                 evil-numbers
+                                 ascope
+                                 ) prelude-packages))
+
+
+;; Install my packages
+(prelude-install-packages)
