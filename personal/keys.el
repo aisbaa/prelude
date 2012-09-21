6981262930ef9a7228291eba7dfd3514b3d18057
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2012-09-21 17:15:17 +0300

    Adds eval and replace functionality, also binds f1 key to woman.

--- personal/keys.el
+++ personal/keys.el
@@ -6,13 +6,35 @@
 
 (global-set-key (kbd "C-x t ") 'aisbaa-insert-time)
 
+
+;;; woman
+(global-set-key (kbd "<f1>") 'woman)
+
+
 ;;; evil numbers
 (global-set-key (kbd "C-+ ") 'evil-numbers/inc-at-pt)
 (global-set-key (kbd "C-- ") 'evil-numbers/dec-at-pt)
+
 
 ;;; redo+
 (require 'redo+)
 (global-set-key (kbd "M-_") 'redo)
 
+
 ;;; recompile
 (global-set-key (kbd "C-c C-r") 'recompile)
+
+
+;;; evaluate Emacs lisp and replace it with result
+(defun fc-eval-and-replace ()
+  "Replace the preceding sexp with its value, f.e. (+ 1 1 1)
+would be replaces with 3."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
+(global-set-key (kbd "C-x e") 'fc-eval-and-replace)
