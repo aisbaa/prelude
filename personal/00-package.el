d52a00c698e4bd3dc384ced296a5066fe2387549
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2012-09-30 22:02:07 +0300

    Renames package.el to 00-package.el adds sr-speedbar to packages.

---
+++ personal/00-package.el
@@ -0,0 +1,11 @@
+;; My packages
+(setq prelude-packages (append '(
+                                 ascope
+                                 evil-numbers
+                                 redo+
+                                 sr-speedbar
+                                 ) prelude-packages))
+
+
+;; Install my packages
+(prelude-install-packages)
--- personal/package.el
+++
@@ -1,9 +0,0 @@
-;; My packages
-(setq prelude-packages (append '(redo+
-                                 evil-numbers
-                                 ascope
-                                 ) prelude-packages))
-
-
-;; Install my packages
-(prelude-install-packages)
