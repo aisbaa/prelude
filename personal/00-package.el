fb24038258a890f8e4a5272ce74da9e05d6db93f
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2012-11-05 15:29:21 +0200

    Added few packages, including http server for emacs.

--- personal/00-package.el
+++ personal/00-package.el
@@ -1,10 +1,13 @@
 ;; My packages
-(setq prelude-packages (append '(ascope
+(setq prelude-packages (append '(
+                                 ascope
                                  diminish
                                  evil-numbers
                                  redo+
                                  revive
+                                 simple-httpd
                                  sr-speedbar
+                                 ssh-config-mode
                                  window-layout
                                  ) prelude-packages))
 
