98938d8d1e0fb46e1df3e0110c70391af309d18c
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2012-12-19 21:33:36 +0200

    Doesnt work yet.

--- personal/java.el
+++ personal/java.el
@@ -1,20 +1,29 @@
-;;; Adds eclim to java mode
-
-;; provides java-mode-hook
-(require 'cc-mode)
-
-
 (add-to-list 'load-path
              (expand-file-name "third_party/eclim" prelude-personal-dir))
 (add-to-list 'load-path
              (expand-file-name "third_party/eclim/vendor" prelude-personal-dir))
 
+(add-to-list 'load-path (expand-file-name "/path/to/emacs-eclim/"))
+;; only add the vendor path when you want to use the libraries provided with emacs-eclim
+(add-to-list 'load-path (expand-file-name "~/coding/git/emacs-eclim/vendor"))
+(require 'eclim)
 
-(add-to-list 'java-mode-hook
-             (lambda ()
-               ;; adds eclim to load path
-               (require 'eclim)
+(setq eclim-auto-save nil)
+(global-eclim-mode 1)
 
-               ;; adds eclim completion to auto complete list
-               (require 'ac-emacs-eclim-source)
-               (add-to-list 'ac-sources 'ac-source-emacs-eclim)))
+
+(setq help-at-pt-display-when-idle t)
+(setq help-at-pt-timer-delay 0.1)
+(help-at-pt-set-timer)
+
+;; regular auto-complete initialization
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; add the emacs-eclim source
+(require 'ac-emacs-eclim-source)
+(ac-emacs-eclim-config)
+
+(add-to-list 'java-mode-hook (lambda ()
+                               (setq inexpr-class 0)
+                               (eclim-mode t)))
