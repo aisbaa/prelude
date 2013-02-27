9abe1a7ed9eef1ec0f369da651f59dabe2a38697
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2013-02-27 18:55:08 +0200

    Adds el-get.

--- personal/00-package.el
+++ personal/00-package.el
@@ -22,5 +22,19 @@
       (append prelude-auto-install-alist
               '(("\\.cs\\'" csharp-mode))))
 
-;; Install my packages
+;; Installs my packages
 (prelude-install-packages)
+
+
+;;; el-get
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync)
