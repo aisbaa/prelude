8b000e187382ae4701a5cf20479cdebb59834255
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2013-03-21 15:09:41 +0200

    Removes el-get.

--- personal/00-package.el
+++ personal/00-package.el
@@ -24,17 +24,3 @@
 
 ;; Installs my packages
 (prelude-install-packages)
-
-
-;;; el-get
-
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(el-get 'sync)
