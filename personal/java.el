2dc320091c6dff42e5192d5151ab249df451c802
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2013-05-14 11:52:33 +0300

    Updates java mode initialisation.

--- personal/java.el
+++ personal/java.el
@@ -1,14 +1,30 @@
 ;; loads java-mode-hook
 (require 'cc-mode)
 
-(setq eclim-executable "~/opt/eclipse/eclim")
+(setq eclim-executable "/usr/share/eclipse/plugins/org.eclim_2.2.5/bin/eclim")
 
-(setq eclimd-default-workspace "~/dev")
+(setq eclimd-default-workspace "~/workspace")
+
+(setq aisbaa-eclim-initialised nil)
+
+(defun aisbaa-init-eclim ()
+  (unless aisbaa-eclim-initialised
+    (message "aisbaa-java::initialising eclim")
+
+   (require 'eclim)
+   (require 'eclimd)
+   (eclim-mode t)
+
+   ;; add the emacs-eclim source
+   (require 'ac-emacs-eclim-source)
+   (ac-emacs-eclim-config)
+
+   (setq aisbaa-eclim-initialised t)))
+
 
 (add-to-list 'java-mode-hook (lambda ()
                                ;; eclim
-                               (require 'eclim)
-                               (eclim-mode t)
+                               (aisbaa-init-eclim)
 
                                ;; compile on save off
                                (compile-on-save-mode nil)
