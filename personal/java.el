b6239f21f390f996c87d2190965c62065965d89e
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2013-11-07 13:20:28 +0200

    Removes eclim stuff, sets tab based indentation to java mode.

--- personal/java.el
+++ personal/java.el
@@ -1,34 +1,8 @@
 ;; loads java-mode-hook
 (require 'cc-mode)
 
-(setq eclim-executable
-      "/usr/share/eclipse/plugins/org.eclim_2.2.5/bin/eclim")
-
-(setq eclimd-default-workspace "~/workspace")
-
-(setq aisbaa-eclim-initialised nil)
-
-(defun aisbaa-init-eclim ()
-  (unless aisbaa-eclim-initialised
-    (message "aisbaa-java::initialising eclim")
-
-   (require 'eclim)
-   (require 'eclimd)
-   (eclim-mode t)
-
-   ;; add the emacs-eclim source
-   (require 'ac-emacs-eclim-source)
-   (ac-emacs-eclim-config)
-
-   (setq aisbaa-eclim-initialised t)))
-
-
 (add-to-list 'java-mode-hook (lambda ()
-                               ;; eclim
-                               ;; (aisbaa-init-eclim)
-
-                               ;; compile on save off
-                               (compile-on-save-mode nil)
+                               (setq indent-tabs-mode t)
 
                                (set-fill-column 100)
 
