3e24ed2e8d2e06d818a9b0e9dfb8099a8563725e
Aistis Jokubauskas <aistis.jokubauskas@headforwards.com>
2015-05-05 13:09:01 +0100

    Adds spartparens mode as global.

--- personal/ui.el
+++ personal/ui.el
@@ -5,6 +5,10 @@
 (setq flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
 
+;; smartparren
+(smartparens-global-mode t)
+
+
 ;;; whitespace cleanup settings.
 (ac-set-trigger-key "TAB")
 (define-key ac-mode-map (kbd "M-SPC") 'auto-complete)
