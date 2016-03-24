165b71874dbe03a56f1eb598c4bb78dcfa02835e
Aistis Jokubauskas <aistis.jokubauskas@headforwards.com>
2016-03-24 15:54:25 +0000

    Set pandoc as default markdown processor

--- personal/00-a-global-variables.el
+++ personal/00-a-global-variables.el
@@ -2,6 +2,7 @@
 
 (provide '00-a-global-variables)
 
-(set-face-attribute 'default nil :height 80)
+(set-face-attribute 'default nil :height 100)
+(setq markdown-command "pandoc")
 
 ;; (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
