2ab081165094f8a9219e220cb61ac753ccf31b83
Aistis Jokubauskas <aistis.jokubauskas@headforwards.com>
2016-06-24 15:05:10 +0100

    Refactor python settings

--- personal/python.el
+++ personal/python.el
@@ -1,26 +1,24 @@
 (require 'flymake-python-pyflakes)
 
-(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-
-(add-hook 'python-mode-hook (lambda () 
-                              (auto-complete-mode t)
-                              (whitespace-mode t)
-                              (compile-on-save-mode)
-                              (require 'sphinx-doc)
-                              (sphinx-doc-mode t)
-                              (flyspell-prog-mode)
-                              (rainbow-delimiters-mode t)
-                              )
-          )
+(add-hook 'python-mode-hook
+          (lambda ()
+            (auto-complete-mode t)
+            (whitespace-mode t)
+            (compile-on-save-mode)
+            (require 'sphinx-doc)
+            (sphinx-doc-mode t)
+            (flyspell-prog-mode)
+            (rainbow-delimiters-mode t)
+            (flymake-python-pyflakes-load)))
 
 (setq flymake-python-pyflakes-executable "flake8")
 
-(add-hook 'compilation-mode-hook (lambda ()
-                                   (highlight-regexp "ERROR" 'hi-red-b)
-                                   (highlight-regexp "FAILED" 'hi-red-b)
-                                   (highlight-regexp "PASSED" 'hi-green-b)
-                                   (highlight-regexp "Assertion Failed:" 'hi-pink)
-                                   ))
+(add-hook 'compilation-mode-hook
+          (lambda ()
+            (highlight-regexp "ERROR" 'hi-red-b)
+            (highlight-regexp "FAILED" 'hi-red-b)
+            (highlight-regexp "PASSED" 'hi-green-b)
+            (highlight-regexp "Assertion Failed:" 'hi-pink)))
 
 ;; setting aspell for 
 (if (string-equal system-type "darwin")
