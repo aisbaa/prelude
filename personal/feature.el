bcaf9a49660f77d8f223efeae42415f18975f3a3
Aistis Jokubauskas <aistis.jokubauskas@headforwards.com>
2016-11-04 10:47:35 +0000

    Add flyspell minor mode to feature mode.

--- personal/feature.el
+++ personal/feature.el
@@ -1,6 +1,7 @@
 (require 'feature-mode)
+
 (add-to-list 'feature-mode-hook (lambda ()
+                                  (flyspell-mode t)
                                   (whitespace-mode t)
                                   (compile-on-save-mode)
-                                  )
-             )
+                                  ))
