217be0647f8ea1b81f401542e1a448fa4e1c4ab8
Aistis Jokubauskas <aistis.jokubauskas@gmail.com>
2018-02-11 11:14:36 +0200

    Remove java and javascript outdated settings

--- personal/javascript.el
+++
@@ -1,7 +0,0 @@
-(require 'js2-mode)
-
-(add-hook 'js-mode-hook (lambda ()
-                          (auto-complete-mode t)
-                          (compile-on-save-mode)
-                          (flyspell-prog-mode)
-                          (whitespace-mode t)))
