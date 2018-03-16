f36e4e3005a46c94e3e62043a34db4fcfe45c404
Aistis Jokubauskas <aistis@uber.com>
2018-03-16 10:03:20 +0200

    Refactor/fix go mode initialization

--- personal/golang.el
+++ personal/golang.el
@@ -1,10 +1,16 @@
 (add-hook 'go-mode-hook (lambda ()
+                          ;; formating options
                           (setq-local indent-tabs-mode t)
                           (setq-local whitespace-style
                                       '(face trailing lines-tail empty indentation space-after-tab space-before-tab))
                           (whitespace-mode t)
 
+                          ;; helper modes
                           (compile-on-save-mode)
                           (flyspell-prog-mode)
-                          (setq-local  indent-tabs-mode t)
-                          (rainbow-delimiters-mode t)))
+                          (rainbow-delimiters-mode t)
+
+                          ;; completion
+                          (require 'company-go)
+                          (require 'go-guru)
+                          ))
