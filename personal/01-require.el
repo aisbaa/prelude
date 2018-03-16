20ee510534ef5fa059d27e4547b76173c9607804
Aistis Jokubauskas <aistis@uber.com>
2018-03-16 10:02:25 +0200

    Refactor required (preloaded) modes

--- personal/01-require.el
+++ personal/01-require.el
@@ -8,17 +8,12 @@
 
 ;;; Code:
 
-(require 'auto-complete)
-(require 'auto-complete-config)
 (require 'buffer-move)
-;; (require 'cmake-mode)
 (require 'flyspell)
 (require 'guru-mode)
-(require 'linum-off)
 (require 'rainbow-mode)
 (require 'undo-tree)
 (require 'whitespace)
-(require 'yasnippet)
 
 (provide '01-require)
  ;;; 01-require.el
