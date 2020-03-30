fdbfb5cfba2d51bffac0e762a9f729ce765bde63
Aistis Jokubauskas <aistis@uber.com>
2020-03-30 12:33:53 +0300

    Enable go lang mode

--- personal/golang.el
+++ personal/golang.el
@@ -1,28 +1,28 @@
-;; (add-hook 'go-mode-hook (lambda ()
-;;                           ;; formating options
-;;                           (setq-local indent-tabs-mode t)
-;;                           (setq-local whitespace-style
-;;                                       '(face trailing lines-tail empty indentation space-after-tab space-before-tab))
-;;                           (setq-local whitespace-line-column 100)
-;;                           (whitespace-mode t)
+(add-hook 'go-mode-hook (lambda ()
+                          ;; formating options
+                          (setq-local indent-tabs-mode t)
+                          (setq-local whitespace-style
+                                      '(face trailing lines-tail empty indentation space-after-tab space-before-tab))
+                          (setq-local whitespace-line-column 100)
+                          (whitespace-mode t)
 
-;;                           ;; helper modes
-;;                           (add-hook 'before-save-hook 'gofmt-before-save)
-;;                           (compile-on-save-mode)
-;;                           (flyspell-prog-mode)
-;;                           (rainbow-delimiters-mode t)
-;;                           (turn-on-fci-mode)
+                          ;; helper modes
+                          (add-hook 'before-save-hook 'gofmt-before-save)
+                          (compile-on-save-mode)
+                          ;; (flyspell-prog-mode)
+                          (rainbow-delimiters-mode t)
+                          ;; (turn-on-fci-mode)
 
-;;                           (require 'go-guru)
+                          ;; (require 'go-guru)
 
-;;                           ;; completion
-;;                           (require 'company-go)
-;;                           (set (make-local-variable 'company-backends) '(company-go))
+                          ;; completion
+                          ;; (require 'company-go)
+                          ;; (set (make-local-variable 'company-backends) '(company-go))
 
-;;                           ;; linters
-;;                           (require 'flycheck)
-;;                           (flycheck-mode t)
-;;                           ))
+                          ;; ;; linters
+                          ;; (require 'flycheck)
+                          ;; (flycheck-mode t)
+                          ))
 
 
 
