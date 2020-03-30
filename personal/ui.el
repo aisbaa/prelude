f1219e91db64dda3df3983f68c9ce7a5c5afface
Aistis Jokubauskas <aistis@uber.com>
2020-03-30 12:50:55 +0300

    Remove duplicate multicursor settings

--- personal/ui.el
+++ personal/ui.el
@@ -47,14 +47,6 @@
           (lambda ()
             (setq display-buffer-reuse-frames t)))
 
-
-;; multicursor
-(require 'multiple-cursors)
-
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
 ;;; diminish mode line
 (require 'diminish)
 
