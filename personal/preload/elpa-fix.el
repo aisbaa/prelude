0fd3a076b331fc366dffacc3cc1589d99cbed3da
Aistis Jokubauskas <aistis@uber.com>
2020-08-17 11:56:43 +0000

    Move elpa fix to preload dir

---
+++ personal/preload/elpa-fix.el
@@ -0,0 +1,3 @@
+;; fix missing gnu archive on debian buster, ref: https://emacs.stackexchange.com/questions/51721/failed-to-download-gnu-archive
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
